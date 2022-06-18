30dba18128c09449a80e11aa82ff1fef10aa40b1
강도훈 <fghfgh3255@gmail.com>
2022-06-18 14:53:44 +0900

    Initial commit

---
+++ app/src/main/java/com/dohn/keepthetime_apipractice_220618/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.dohn.keepthetime_apipractice_220618
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
